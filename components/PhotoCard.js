71b76ba8b216fe9d8e1acd8ad7e7755070333bb1
Dustin Yelland <dyelland@gmail.com>
2023-07-10 15:19:21 -0500

    styling for album page

--- components/PhotoCard.js
+++ components/PhotoCard.js
@@ -1,14 +1,14 @@
 import Image from "next/image";
-import styles from "../styles/AlbumCard.module.css";
+import styles from "../styles/PhotoCard.module.css";
 
 const PhotoCard = ({ id, url, title }) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.content} data-testid={`photo-card-${id}`}>
-        <div className={styles.imgWrapper}>
-          <Image alt={title} src={url} height="600" width="600" />
-        </div>
-        <div data-testid="photo-title">{title}</div>
+    <div className={styles.content} data-testid={`photo-card-${id}`}>
+      <div>
+        <Image alt={title} src={url} height="600" width="600" />
+      </div>
+      <div className={styles.titleContainer} data-testid="photo-title">
+        <div className={styles.title}>{title}</div>
       </div>
     </div>
   );
