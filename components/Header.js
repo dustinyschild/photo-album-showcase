9a0c0d8f23a1689c64a171cddc328f2ada415881
Dustin Yelland <dyelland@gmail.com>
2023-07-10 11:04:44 -0500

    some styling for the header

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,24 @@
+import styles from "../styles/Header.module.css";
+
 const Header = () => {
   return (
-    <header data-testid="header">
-      <div>
-        <a data-testid="linkedin-link" href="#">
-          LinkedIn
-        </a>
-        <a data-testid="github-link" href="#">
+    <header className={styles.header} data-testid="header">
+      <div className={styles.linkBar}>
+        <a className={styles.socialLink} data-testid="github-link" href="#">
           Github
+        </a>
+        <a className={styles.socialLink} data-testid="linkedin-link" href="#">
+          LinkedIn
         </a>
       </div>
 
-      <h1 data-testid="main-heading">Photo Album Showcase</h1>
-      <h2 data-testid="sub-heading"> by Dustin Yelland</h2>
+      <h1 className={styles.mainHeader} data-testid="main-heading">
+        Photo Album Showcase
+      </h1>
+      <h2 className={styles.subHeader} data-testid="sub-heading">
+        {" "}
+        by Dustin Yelland
+      </h2>
     </header>
   );
 };
