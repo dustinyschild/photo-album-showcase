99a74ca89c61b85db8f199fb36d838a388f4fb92
Dustin Yelland <dyelland@gmail.com>
2023-07-12 10:44:47 -0500

    test updates for album card redesign

--- __tests__/components/AlbumCard.test.js
+++ __tests__/components/AlbumCard.test.js
@@ -10,30 +10,75 @@ const albumCardData = {
   thumbnailUrl: "https://via.placeholder.com/150/92c952"
 };
 
+const photoData = [
+  {
+    albumId: 1,
+    id: 1,
+    title: "accusamus beatae ad facilis cum similique qui sunt",
+    url: "https://via.placeholder.com/600/92c952",
+    thumbnailUrl: "https://via.placeholder.com/150/92c952"
+  },
+  {
+    albumId: 1,
+    id: 2,
+    title: "reprehenderit est deserunt velit ipsam",
+    url: "https://via.placeholder.com/600/771796",
+    thumbnailUrl: "https://via.placeholder.com/150/771796"
+  },
+  {
+    albumId: 1,
+    id: 3,
+    title: "officia porro iure quia iusto qui ipsa ut modi",
+    url: "https://via.placeholder.com/600/24f355",
+    thumbnailUrl: "https://via.placeholder.com/150/24f355"
+  },
+  {
+    albumId: 1,
+    id: 4,
+    title: "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+    url: "https://via.placeholder.com/600/d32776",
+    thumbnailUrl: "https://via.placeholder.com/150/d32776"
+  },
+  {
+    albumId: 1,
+    id: 5,
+    title: "natus nisi omnis corporis facere molestiae rerum in",
+    url: "https://via.placeholder.com/600/f66b97",
+    thumbnailUrl: "https://via.placeholder.com/150/f66b97"
+  }
+];
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve(photoData)
+  })
+);
+
 describe("AlbumCard", () => {
-  it("should link to album", () => {
-    render(<AlbumCard {...albumCardData} />);
-
-    const albumLink = screen.getByRole("link");
-
-    expect(albumLink.href).toBe("http://localhost/album/1");
-  });
-
-  it("should contain the first photo's thumbnail", () => {
-    render(<AlbumCard {...albumCardData} />);
-
-    const image = screen.getByRole("img");
-
-    expect(image).toBeInTheDocument();
-  });
-
-  // I would have prefered an album title but that doesn't exist in this data set
-  it("should show the albumId", () => {
+  it("should show the album title (albumId)", () => {
     render(<AlbumCard {...albumCardData} />);
 
     const albumName = screen.getByTestId("album-title");
 
     expect(albumName).toBeInTheDocument();
     expect(albumName.textContent).toBe("Album 1");
+  });
+
+  // mock the data for hook
+  it("should show up to 5 album photos", () => {
+    render(<AlbumCard {...albumCardData} />);
+
+    const photos = screen.queryAllByRole("img");
+
+    expect(photos).toHaveLength(5);
+  });
+
+  it("should have a See All link", () => {
+    render(<AlbumCard {...albumCardData} />);
+
+    const albumLink = screen.getByRole("link");
+
+    expect(albumLink.href).toBe("http://localhost/album/1");
+    expect(albumLink.textContent).toContain("See All");
   });
 });
--- components/AlbumCard.js
+++ components/AlbumCard.js
@@ -1,8 +1,22 @@
 import Image from "next/image";
 import styles from "../styles/AlbumCard.module.css";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const AlbumCard = ({ albumId, thumbnailUrl, title }) => {
+  const [albumPhotos, setAlbumPhotos] = useState();
+
+  useEffect(() => {
+    const getFirstFivePhotos = async () =>
+      fetch(
+        `https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`
+      ).then((res) => res.json());
+
+    getFirstFivePhotos().then((data) => {
+      setAlbumPhotos(data.slice(0, 5));
+    });
+  }, [albumId]);
+
   return (
     <div className={styles.container}>
       <div className={styles.content} data-testid={`album-card-${albumId}`}>
